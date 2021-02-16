ee8764161f0e1b908185b6156a5847ae49df8354
luchob <lachezar.balev@gmail.com>
2021-02-16 19:00:15 +0200

    Strip the sample.

--- rest/src/main/java/bg/sofunit/rest/web/AuthorsNamespace.java
+++
@@ -1,8 +0,0 @@
-package bg.sofunit.rest.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping(AuthorsNamespace.URI_AUTHORS)
-public interface AuthorsNamespace {
-  String URI_AUTHORS = "/authors";
-}
