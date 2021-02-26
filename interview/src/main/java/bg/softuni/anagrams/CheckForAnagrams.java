91a0f814a095090de985d4ed41c77c9391f97c8c
rd-lachezar-balev <lachezar.balev@rewe-digital.com>
2021-02-26 18:07:57 +0200

    Anagrams.

---
+++ interview/src/main/java/bg/softuni/anagrams/CheckForAnagrams.java
@@ -0,0 +1,16 @@
+package bg.softuni.anagrams;
+
+import java.util.List;
+
+public class CheckForAnagrams {
+
+  public void listAnagrams(List<String> words) {
+    // TODO:
+    // List all words that are anagrams.
+    // Example: бира, риба, кавалер, акварел, пешо, софтуни
+    // Expected output:
+    // бира, риба
+    // кавалер, акварел
+  }
+
+}
