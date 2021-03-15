75114de4914fe51bf58ec393b062ae633b020413
luchob <lachezar.balev@gmail.com>
2021-03-15 20:12:20 +0200

    mapstruct demo

---
+++ mapstuct/src/main/java/bg/softuni/mapstuct/MapstuctApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.mapstuct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstuctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstuctApplication.class, args);
+	}
+
+}
---
+++ mapstuct/src/test/java/bg/softuni/mapstuct/MapstuctApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.mapstuct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstuctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
